8ed65fad8fc62b55e09e2c8e9f9f394ade19a0d7
Gump-TUF <Gump1016@163.com>
2020-01-15 22:19:08 +0800

    next中的路由:最基础的Link标签的应用

---
+++ pages/page1.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const page1 = () => (
+  <div>
+    <span>ProbeDream page1</span>
+    <Link href="/">back home!</Link>
+  </div>
+);
+
+export default page1;
---
+++ pages/page2.js
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+const page2 = () => (
+  <div>
+    <span>ProbeDream page2</span>
+    <Link href="/">back home</Link>
+  </div>
+);
+
+export default page2;
