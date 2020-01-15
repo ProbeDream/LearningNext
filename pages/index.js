579fb59ce8493083c7c91496670ca5fe83845910
Gump-TUF <Gump1016@163.com>
2020-01-15 22:33:32 +0800

    添加编程式路由的方式!

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,25 @@
-import React from 'react'
-import Link from "next/link";
-import Title from '../components/title'
- 
+import React from 'react';
+import Link from 'next/link';
+import Title from '../components/title';
+import Router from "next/router";
 
 const Home = () => (
   <div>
-     <Title>ProbeDream</Title> 
-     <div><Link href="/page1"><a>go to page1!</a></Link></div>
-     <div><Link href="/page2"><a>go to page2!</a></Link></div>
-  </div>
-)
+    <Title>ProbeDream</Title>
+    <div>
+      <Link href="/page1">
+        <a>go to page1!</a>
+      </Link>
+    </div>
+    <div>
+      <Link href="/page2">
+        <a>go to page2!</a>
+      </Link>
+    </div>
 
-export default Home
+    <button onClick={()=>{Router.push("/page1")}}>goA</button>
+    <button onClick={()=>{Router.push("/page2")}}>goB</button>
+  </div>
+);
+
+export default Home;
